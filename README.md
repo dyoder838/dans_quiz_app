f65d3e666f8b8619b2752c2c017ddb44656970ad
dyoder838 <70074229+dyoder838@users.noreply.github.com>
2020-09-16 23:10:21 -0700

    updated arrays, added score keeping code, refactored, edited readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # dans_quiz_app
+
+This app is a real benchmark test of my coding ability as I progress through my course in web development; it has been a real challenge and a huge learning opportunity.
+
+the goal is to make a timed quiz. 
+
+the rules are simple, a timer starts when the quiz starts. 
+ - the player has 60 seconds to finish the quiz.
+ - 5 seconds are subtracted for every incorrect answer.
+ - points are tallied at the end of the quiz and given a score equal to correct answers per second multiplied by 1000 - because who likes low numbers
+ - the user gets to enter their initials next to their score and the score remains in local storage
+
+coding this required use of:
+
+javascript
+jquery
+css
+arrays
+for loops
+local storage
+intervals
+if else statements 
+
+at the time of writing this, the project has yet to be completed, one can follow my progress using my github repository:
+https://github.com/dyoder838/dans_quiz_app.git
+
+One can see the app in action here:
+https://dyoder838.github.io/dans_quiz_app/.
+
+
--- script.js
+++ script.js
@@ -3,11 +3,8 @@
 // ------------- buttons ------------------
 const startButton = $("#start-button")
 const stopButton = $("#stop-button")
-const aButton = $("#a-button")
-const bButton = $("#b-button")
-const cButton = $("#c-button")
-const dButton = $("#d-button")
-
+const nextButton = $("#next-button")
+nextButton.hide()
 //------------- master clock ------------
 const minutes = $("#master-clock-minutes")
 const seconds = $("#master-clock-seconds")
@@ -20,14 +17,47 @@ var secondsLeft = 5;
 var timeInterval;
 // ----------- main window --------
 const mainBox = $("#variable-box")
+const quizMessage = $("#quiz-message")
 
 // ---------- Question ------------
-const question = $("#question")
+const questionBox = $("#question-box")
+const choiceList = $("#choice-list")
 
 // ---------- running tallies -------
 const qRemain = $("#questions-remaining")
-const qCorrect = $("#questions-Correct")
+const qCorrect = $("#questions-correct")
 const altFacts = $("#alternative-facts")
+
+var currentQuestion = 0;
+
+var correctAnswers = 1;
+
+var alternateFacts = 1;
+
+var quizOver = false;
+
+var questionArr = [
+    {
+        "question": "Who is Cloud?",
+        "answers": ["Main character", "Main bad guy", "Supporting character", "Evaporated water"],
+        "correctAnswer": 0
+    }, 
+     {
+        "question": "What is Materia used for?",
+        "answers": ["Materia gives you special abilities", "Materia makes characters explode", "Materia are in game currency", "Materia is useless"],
+        "correctAnswer": 0
+    }, 
+    {
+        "question": "Who is Sephiroth",
+        "answers": ["Cloud's brother", "A shopkeeper from the first act", "The main bad guy", "The creator of the game"],
+        "correctAnswer": 2
+    }, 
+    {
+        "question": "in the original release of the game on the PlayStation, how many disks did the came use? ",
+        "answers": ["1", "2", "3", "4"],
+        "correctAnswer": 2
+    }
+];
 
 // GIVEN I am taking a code quiz
 // WHEN I click the start button ,THEN a timer starts and I am presented with a question
@@ -38,22 +68,25 @@ $(startButton).on("click", function(){
     // 5 second count down w text in question
     startDelayClock();
     console.log("button working?:" , startButton)
+  
 });
 
 // ------ start button triggers delay start count down ----------
 function startDelayClock(){
-    
+   
     var timerInterval = setInterval(function() {
     
-        question.text("Begin quiz in "  + secondsLeft);
+        questionBox.text("Begin quiz in "  + secondsLeft);
         secondsLeft--;
         console.log("does startDelayClock work:" , secondsLeft)
         if(secondsLeft === 0) {
             clearInterval(timerInterval);
-            question.empty()
+            questionBox.empty()
             startMasterClock();
             startQuiz()
         }
+        
+            
     }, 1000);
 };
  
@@ -76,47 +109,128 @@ function startMasterClock(){
 
 // when timer starts counting the multiple choice field appears
             // function that populates the dom object mainBox, makes the questions change
-// --------- questions and answers -----------
-// how to construct the array
- //// create an external index that can cycle the questions i++ 
 
- 
-var myArr = [
-    {
-        "question": "Who is Cloud?",
-        "answers": ["Main character", "Main bad guy", "Supporting character", "Evaporated water"],
-        "correctAnswer": "Main character"
-    }, 
-     {
-        "question": "What is Materia used for?",
-        "answers": ["Materia gives you special abilities", "Materia makes characters explode", "Materia are in game currency", "Materia is useless"],
-        "correctAnswer": "Materia gives you special abilities"
-    }, 
-    {
-        "question": "Who is Sephiroth",
-        "answers": ["Cloud's brother", "A shopkeeper from the first act", "The main bad guy", "The creator of the game"],
-        "correctAnswer": "The main bad guy"
-    }, 
-    {
-        "question": "in the original release of the game on the PlayStation, how many disks did the came use? ",
-        "answers": ["1", "2", "3", "4"],
-        "correctAnswer": "3"
-    }
-];
- 
- // my array, question (object) indexed at 0, and of that, answers indexed, call them with 0- whatever 
- // use these as links to the main viewer - try to make a template with a variable that progresses
- //through the array with a for loop
- console.log(myArr[0].answers[1])
-
-
-        
 // TODO: build the multiple choice fields
-    //we need to construct arrays that pertain to the dom ids #question-box and a-d buttons
-        // there needs to be a && if function to determine the correct answer. 
-        // there needs to be a tracker that tallies wright wrong and remaining questions
-            // when the question is answered wrong, 5 seconds is subtracted from the main clock
+            
+                        // --------- questions and answers -----------
+ 
 
+
+
+// variables to store question values
+
+
+// this function runs along side the clock, and is the start of the quiz
+function startQuiz() {
+
+// make a for loop to cycle through the array and pull out the information we need, and display them:
+    //1 the current question, 2 the answers the user can select, 3 the correct answer
+            //note, as these all belong to the same object, current question is inherently associated with answers
+
+    // --- remember dom references --> 
+    //const questionBox = $("#question-box")
+    //const choiceList = $("#choice-list")
+    
+    //pulls question from array - the current question to displayed
+    var question = questionArr[currentQuestion].question;
+    // Set the questionBox text to the current question
+    $(questionBox).text(question);
+    
+    // Remove all current <li> elements (if any) - because we are making some below, and we need to refresh
+    $(choiceList).find("li").remove();
+
+// pull the answers out of the answers index 
+    
+    //gives the length of the answers array within questionArr
+    var answersIndex = questionArr[currentQuestion].answers.length;
+    // these are the answers the user can select - generated with the for loop
+    var answers;
+    $(qRemain).text(questionArr.length - currentQuestion)
+    //i is a progressive index number starting at 0 and ending with the end of the list
+    
+    for (i = 0; i < answersIndex; i++) {
+        //this will pull out every answer to its parent questionArr index (answers are already associated with questions)
+        answers = questionArr[currentQuestion].answers[i];
+        // this will create li's in choiceList in the amount of answers in the list, because the 
+        //for loop pushes the whole list of answers, in this case 4
+        $('<li><input type="radio" value=' + i + ' name="dynradio" />' + answers + '</li>').appendTo(choiceList);
+    }
+    nextButton.show()
+};
+
+//push the next button to do it again
+ 
+    //const nextButton = $("#next-button")
+
+    $(nextButton).on("click", function () {
+        $(altFacts).text(alternateFacts)
+        $(qCorrect).text(correctAnswers)
+        if (!quizOver) {
+
+            value = $("input[type='radio']:checked").val();
+
+            if (value == undefined) {
+                $(quizMessage).text("You didn't pick anything!!");
+                $(quizMessage).show();
+            } else {
+                
+                $(quizMessage).hide();
+
+                if (value == questionArr[currentQuestion].correctAnswer) {
+                    correctAnswers++;
+                    console.log("do correct answers increase:" , correctAnswers)
+                } else {
+                    alternateFacts++;
+                }
+
+                
+                // Since we have already displayed the first question on DOM ready
+                // currentQuestion++;
+                if (currentQuestion < questionArr.length) {
+                    startQuiz();
+                } else {
+                    displayScore();
+                    // $(document).find(".nextButton").toggle();
+                    // $(document).find(".playAgainButton").toggle();
+                    // Change the text in the next button to ask if user wants to play again
+                    $(nextButton).text("Play Again?");
+                    quizOver = true;
+                }
+            }
+        } else { // quiz is over and clicked the next button (which now displays 'Play Again?'
+            quizOver = false;
+            $(nextButton).text("Next Question");
+            resetQuiz();
+            startQuiz();
+            hideScore();
+        }
+    });
+
+    // $(stopButton).on("click", function(){
+    //     resetQuiz(),
+    //     console.log(this)
+    //     return;
+    // });
+
+    function resetQuiz() {
+        currentQuestion = 0;
+        correctAnswers = 0;
+    
+    };
+    
+    function displayScore() {
+        questionBox.empty()
+        choiceList.empty()
+        questionBox.text("You scored: " + correctAnswers + " out of: " + questionArr.length);
+        
+        
+    };
+    
+    // function hideScore() {
+    //     $(document).find(".result").hide();
+    // };     
+    
+    
 // WHEN all questions are answered or the timer reaches, THEN the game is over
     // create an if, (clock.value = 0), if(quiz.length = 10) then launch score menu
 
